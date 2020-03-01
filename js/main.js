a264d798eb7e2403855bf3b65ddf6d42a9aeffdf
Vadim <50245404+Zitronius@users.noreply.github.com>
2020-03-01 10:33:51 +0300

    Первая загрузка файлов

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+   $('.rooms__slider').slick({
+    dots: true,
+    fade: true,
+    autoplay: false,
+    autoplaySpeed: 4000,
+    });
+
+    $('.reviews__slider').slick({
+        dots: true,
+        infinite: false,
+    }); 
+});
+
