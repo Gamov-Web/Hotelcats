bdddeb347fb8f75d032b25dd70c70caceb7c2fa3
Vadim <>
2020-03-09 13:18:42 +0300

    Использование gulp

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
    $('.rooms__slider').slick({
-    dots: true,
-    fade: true,
-    autoplay: false,
-    autoplaySpeed: 4000,
+        dots: true,
+        fade: true,
+        autoplay: 3000,
+        prevArrow: $('.slick-prev'),
+        nextArrow: $('.slick-next')
     });
 
     $('.reviews__slider').slick({
         dots: true,
         infinite: false,
+        variableWidth: true,
+        prevArrow: $('.slick-prev-new'),
+        nextArrow: $('.slick-next-new')
     }); 
 });
 
